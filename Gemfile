ee1e327743dc8d2332b26015ba45312bb30f61ce
Bill Israel <bill.israel@gmail.com>
2018-09-09 17:07:17 -0500

    Use github-pages gem, remove custom plugins
    Github Pages won't use custom plugins for security reasons, so best to
    just remove them to avoid confusion.

--- Gemfile
+++ Gemfile
@@ -4,4 +4,4 @@ source "https://rubygems.org"
 
 git_source(:github) {|repo_name| "https://github.com/#{repo_name}" }
 
-gem "jekyll", "3.7.4"
+gem "github-pages", group: :jekyll_plugins
--- _plugins/titlecase.rb
+++
@@ -1,9 +0,0 @@
-require 'liquid'
-
-module TitleCase
-  def title_case(text)
-    return text.split(' ').map(&:capitalize).join(' ')
-  end
-end
-
-Liquid::Template.register_filter(TitleCase)
